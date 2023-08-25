260a219f748d0362cd6d12534545551414f08cf7
Jeancar Sanchez <jeancarleonardo514@gmail.com>
2023-08-25 02:41:56 -0400

    app todo list version 3

--- src/main/resources/import.sql
+++
@@ -1,5 +0,0 @@
--- Agregar tareas de ejemplo
-INSERT INTO task (descripcion, date, completed) VALUES
-                                                      ('Tarea 1', '2023-08-24', false),
-                                                      ('Tarea 2', '2023-08-25', true),
-                                                      ('Tarea 3', '2023-08-26', false);
